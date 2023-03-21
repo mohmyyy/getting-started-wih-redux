3c7212361ff0e03b6e101b87ceff363fac975b61
mohmyyy <iammohmy@gmail.com>
2023-03-21 14:40:13 +0530

    working with dispatch function.

--- redux-demo.js
+++ redux-demo.js
@@ -2,15 +2,22 @@ const redux = require("redux");
 
 const counterReducer = (state = { counter: 0 }, action) => {
   if (action.type === "increment") {
-    console.log("increment");
     return {
-      counter: state.counter + 1,
+      counter: state.counter + 2,
+    };
+  }
+  if (action.type === "incrementby2") {
+    return {
+      counter: state.counter + 2,
     };
   }
   if (action.type === "decrement") {
-    console.log("decrement");
     return { counter: state.counter - 1 };
   }
+  if (action.type === "decrementby2") {
+    return { counter: state.counter - 2 };
+  }
+  return state;
 };
 
 // A reducer function is a standard javascript function, but it will be called by redux library and it will then always receive two pieces of input, two parameters, the old or existing state and the action that was dispatched. and then its reducer function must return a certain output.
@@ -21,13 +28,16 @@ const store = redux.createStore(counterReducer);
 const counterSubscriber = () => {
   const latestState = store.getState();
   //getState() is a method which is available on the store created with create store.
-  console.log(latestState,24);
+  console.log(latestState, 24);
 };
 
 store.subscribe(counterSubscriber);
 store.dispatch({ type: "increment" });
-console.log(store.getState(),'after Icrement');
-store.dispatch({ type: "increment" });
-console.log(store.getState(),'after second Icrement');
+console.log(store.getState(), "increment");
+store.dispatch({ type: "incrementby2" });
+console.log(store.getState(), "increment by 2");
+
 store.dispatch({ type: "decrement" });
-console.log(store.getState());
+console.log(store.getState(),'decrement');
+store.dispatch({ type: "decrementby2" });
+console.log(store.getState(),'decrement by 2');
